8622cfe0dea99ac2dfc886e0c539487f4f93c66a
Ildar_shamaev <IldarShamaev12378@mail.ru>
2021-01-09 22:10:32 +0500

    Добавьте файлы проекта.

---
+++ ТП/Board.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ТП
+{
+    class Board
+    {
+        private int[,] map = new int[8, 8]
+        {
+            {15,14,13,12,11,13,14,15 },
+            {16,16,16,16,16,16,16,16 },
+            {0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0 },
+            {26,26,26,26,26,26,26,26 },
+            {25,24,23,22,21,23,24,25 },
+        };
+
+        private Button[,] butts = new Button[8, 8];
+
+        private int currPlayer;
+
+        public int [,] Map
+        {
+            get => map;
+        } 
+        public Button[,] Butts
+        {
+            get => butts;
+        }
+        public int CurrPlayer
+        {
+            get => currPlayer;
+        }
+        public void CreateMap()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    butts[i, j] = new Button();
+
+                    Button butt = new Button();
+                    butt.Size = new Size(50, 50);
+                    butt.Location = new Point(j * 50, i * 50);
+
+                    switch (map[i, j] / 10)
+                    {
+                        case 1:
+                            Image part = new Bitmap(50, 50);
+                            Graphics g = Graphics.FromImage(part);
+                            g.DrawImage(chessSprites, new Rectangle(0, 0, 50, 50), 0 + 150 * (map[i, j] % 10 - 1), 0, 150, 150, GraphicsUnit.Pixel);
+                            butt.BackgroundImage = part;
+                            break;
+                        case 2:
+                            Image part1 = new Bitmap(50, 50);
+                            Graphics g1 = Graphics.FromImage(part1);
+                            g1.DrawImage(chessSprites, new Rectangle(0, 0, 50, 50), 0 + 150 * (map[i, j] % 10 - 1), 150, 150, 150, GraphicsUnit.Pixel);
+                            butt.BackgroundImage = part1;
+                            break;
+                    }
+                    butt.BackColor = Color.White;
+                    butt.Click += new EventHandler(OnFigurePress);
+                    this.Controls.Add(butt);
+
+                    butts[i, j] = butt;
+                }
+            }
+        }
+    }
+
+}
